5464a08a9a4f88d08462b3ff97a38e64bf0754d9
Nahid Hasan <anonymousnahidhasan@gmail.com>
2025-08-19 21:44:54 +0600

    Created the sections for Brands, Categories and Sliders

--- client/src/components/features/Features.jsx
+++ client/src/components/features/Features.jsx
@@ -3,12 +3,36 @@ import useFeatureStore from "../../store/useFeatureStore";
 import FeaturesSkeleton from "./../../skeleton/FeaturesSkeleton";
 
 const Features = () => {
-  const { featureList } = useFeatureStore();
+  const { featuresList } = useFeatureStore();
 
-  if (featureList === null) {
+  if (featuresList === null) {
     return <FeaturesSkeleton />;
   } else {
-    return <div></div>;
+    return (
+      <div className="container section">
+        <div className="row">
+          {featuresList.map((item, index) => {
+            return (
+              <div key={index} className="col-6 p-2 col-md-2 col-lg-3 col-sm-6">
+                <div className="card shadow-sm">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-3">
+                        <img className="w-100" src={item.img} />
+                      </div>
+                      <div className="col-9">
+                        <h3 className="bodyXLarge">{item.name}</h3>
+                        <span className="bodySmal">{item.description}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 };
 
--- client/src/components/product/Brands.jsx
+++ client/src/components/product/Brands.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useProductStore from "../../store/useProductStore";
 import BrandsSkeleton from "./../../skeleton/BrandsSkeleton";
 
@@ -8,7 +9,31 @@ const Brands = () => {
   if (brandList === null) {
     return <BrandsSkeleton />;
   } else {
-    return <div></div>;
+    return (
+      <div className="section">
+        <div className="container">
+          <div className="row">
+            <h1 className="headline-4 text-center my-2 p-0">Top Brands</h1>
+            <span className="bodySmal mb-5 text-center">
+              Explore a World of Choices Across Our Most Popular <br />
+              Shopping Categories{" "}
+            </span>
+            {brandList.map((item) => (
+              <div className="col-6 col-lg-8r text-center col-md-8r p-2">
+                <Link to={`/by_brand/${item._id}`} className="card h-100 rounded-3 bg-white">
+                  <div className="card-body d-flex flex-column align-items-center justify-content-between">
+                    <div className="d-flex flex-grow-1 align-items-center justify-content-center">
+                      <img alt={item.brandName} className="w-75" src={item.brandImg} />
+                    </div>
+                    <p className="bodySmal mt-3 mb-0">{item.brandName}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 
