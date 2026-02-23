3498bf8ed10c8b072e38428b7cd15fa73116100c
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2026-02-23 19:26:04 +0600

    chore: initial Redux Toolkit setup with baseApi and store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/css/animate.min.css";
 import "./assets/css/main.css";
-import 'react-loading-skeleton/dist/skeleton.css';
+import "react-loading-skeleton/dist/skeleton.css";
 
-
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
+  </Provider>,
 );
